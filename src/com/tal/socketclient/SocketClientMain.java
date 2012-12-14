02e189cb61ff5037a9e894490ef7f05b43f09ab8
Tristan Linnell <talinnell@gmail.com>
2012-12-14 10:18:18 +0000

    Tidy.

--- src/com/tal/socketclient/SocketClientMain.java
+++ src/com/tal/socketclient/SocketClientMain.java
@@ -9,27 +9,24 @@ public class SocketClientMain {
 	 */
 	public static void main(String[] args) {
 		System.out.println("Socket client starting main...");
-		
+
 		try {
 			SocketClient s = new SocketClient("127.0.0.1", 8080);
-			
-			
+
+
 			for(int i=0; i<10; i++) {
 				String outputLine = "GIMME DATA! :-)";		
 				String inputLine = s.getData(outputLine);
-						
+
 				if(inputLine != null) {
 					System.out.println("Reply received from server: " + inputLine);
 				}
 			}
 
-					
 		} catch (IOException e) {
 			System.out.println("Couldn't create client.");
 		}
-	
-	System.out.println("end");
 
+		System.out.println("end");
 	}
-
 }
