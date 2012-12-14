ee9789b8185091f95d3ab0e5c08b25cc70db7adb
Tristan Linnell <talinnell@gmail.com>
2012-12-14 10:05:03 +0000

    Refactor ready for unit testing.
    So that JUnit can test the getting of data from the server.

--- src/com/tal/socketclient/SocketClient.java
+++ src/com/tal/socketclient/SocketClient.java
@@ -4,7 +4,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
-import java.net.ServerSocket;
 import java.net.Socket;
 
 public class SocketClient {
@@ -38,25 +37,17 @@ public class SocketClient {
 	}
 
 	private void startServer() throws IOException{
-		//create the in and out
+		//create the in and out ready to be used
 		out = new PrintWriter(socket.getOutputStream(), true);
 		in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-
-		runServer();
 	}
-
-	private void runServer() throws IOException{
-		String inputLine, outputLine;
-
-		for(int i=0; i<10; i++) {
-			outputLine = "GIMME DATA!";
-			out.println(outputLine);
-			
-			if((inputLine = in.readLine()) != null) {
-				System.out.println("Reply received from server" + socket.getInetAddress() +": " + inputLine);
-			}
-		}
-
+	
+	public String getData(String toSend) throws IOException{
+		out.println(toSend);
+		return in.readLine();
+	}
+	
+	public void close() throws IOException{
 		out.close();
 		in.close();
 		socket.close();
